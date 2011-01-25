975811d2bbc91f9e055cae6f097f5f2242f6bed5
Matthew Esch <mattesch@Matthew-Eschs-Computer-3.local>
2011-01-24 20:58:20 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Badgnet::Application.config.secret_token = 'fb8a834b3185bbe74c5e0ab93398a20d3a1c7458396ba380c3ce4f9cc8954cf9814f7599082f7e72ed1293b842586e3af455955747c5342caa03ac8d2e1e55af'
