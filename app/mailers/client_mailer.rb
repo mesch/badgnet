345ac7ff10255ad55fe2e6b894d5d712e2ca3952
Matthew Esch <mattesch@Matthew-Eschs-Computer-3.local>
2011-02-15 15:55:56 -0800

    more email stuff

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,20 +1,20 @@
 class ClientMailer < BaseMailer
 
-  def self.activation(to, username, activation_code)
+  def activation(to, username, activation_code)
     subject    = "Please activate your account"
     @username   = username
     @activation_code = activation_code
     mail(:to => to, :subject => subject)
   end
 
-  def self.forgot_password(to, username, password)
+  def forgot_password(to, username, password)
     subject    = "Your password is ..."
     @username   = username
     @password   = password
     mail(:to => to, :subject => subject)
   end
   
-  def self.changed_email(to, username, old_email, new_email)
+  def changed_email(to, username, old_email, new_email)
     subject    = "You have changed your email"
     @username   = username
     @old_email  = old_email
